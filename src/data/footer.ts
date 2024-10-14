94c51bfc7148ada1bb00307a5ed0b1fa0a3da4b4
nwabueze1 <padrefidelis111@gmail.com>
2024-10-14 07:23:27 +0100

    refactor code

--- src/data/footer.ts
+++
@@ -1,9 +0,0 @@
-export const footerData = [{
-  title: "View my Project Portfolio",
-  url: "/projects",
-},
-  {
-    title: "Connect with me on LinkedIn",
-    url: "https://www.linkedin.com/in/anslemokeke/",
-  },
-];
